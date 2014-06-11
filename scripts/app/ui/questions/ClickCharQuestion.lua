1f11dc44eec1faf78aa7d66feba1490b2084656f
Hans Zhang <ezburg@gmail.com>
2014-06-11 17:12:03 +0200

    Finished the ClickCharQuestion

--- scripts/app/ui/questions/ClickCharQuestion.lua
+++ scripts/app/ui/questions/ClickCharQuestion.lua
@@ -11,16 +11,26 @@ function ClickCharQuestion:ctor()
     local height = self:getAvailableHeight()
 
     display.addSpriteFramesWithFile("image/crystal_btns.plist", "image/crystal_btns.png")
-    local positions = {
-        { x = 391, y = 429 },
-        { x = 235, y = 190 },
-        { x = 144, y = 590 },
-        { x = 467, y = 673 },
-        { x = 486, y = 192 },
-        { x = 115, y = 334 }
+    self.positions = {
+        {
+            { x = 391, y = 429 },
+            { x = 235, y = 190 },
+            { x = 144, y = 590 },
+            { x = 467, y = 673 },
+            { x = 486, y = 192 },
+            { x = 115, y = 334 }
+        },
+        {
+            { x = 450, y = 200 },
+            { x = 200, y = 390 },
+            { x = 420, y = 590 },
+            { x = 320, y = 280 },
+            { x = 115, y = 192 },
+            { x = 144, y = 634 }
+        }
     }
-    local colors = {'blue', 'green', 'purple', 'red', 'white', 'yellow' }
-    local titleColors = {
+    self.colors = {'blue', 'green', 'purple', 'red', 'white', 'yellow' }
+    self.titleColors = {
         display.COLOR_WHITE,
         display.COLOR_BLACK,
         display.COLOR_WHITE,
@@ -28,42 +38,46 @@ function ClickCharQuestion:ctor()
         ccc3(255, 0, 0),
         display.COLOR_BLACK,
     }
+
+    self.currentIndex = 1
+    self.buttons = {}
+    self:buildLayout()
+end
+
+function ClickCharQuestion:buildLayout()
+    for i = 1, #self.buttons do
+        self.buttons[i]:removeFromParent()
+    end
+    self.buttons = {}
+
     local scales = ez:randomNumber(10, 6, 4)
-
-    local currentIndex = 1
-
     for i = 1, 6 do
-        local position = positions[i]
---        local item = display.newSprite(
---            '#star_' ..(i - 1) .. '.png',
---            position['x'],
---            height * position['y'] / 800
---        )
---        item:setTouchEnabled(true)
---        item:addTouchEventListener(function()
---            transition.execute(item, CCScaleTo:create(0.3, 0.1), {
---                easing = "backIn",
---                onComplete = function()
---                    if i ~= currentIndex then
---                        self:alertError()
---                    else
---                        item:removeFromParent()
---                        currentIndex = currentIndex + 1
---                        if currentIndex == 7 then
---                            self:gotoNextQuestion()
---                        end
---                    end
---                end
---            })
---        end)
+        local title = '' .. i
+        if i == 4 then
+            title = '5'
+        end
+        local position = self.positions[self.currentIndex][i]
         local item = CrystalButton.new({
-            color = colors[i],
-            title = '' .. i,
-            titleColor = titleColors[i],
+            color = self.colors[i],
+            title = title,
+            titleColor = self.titleColors[i],
             scale = scales[i] / 10.0,
             x = position.x,
-            y = position.y
+            y = position.y,
+            listener = function()
+                if i == 5 then
+                    self.currentIndex = self.currentIndex + 1
+                    if self.currentIndex > 2 then
+                        self:gotoNextQuestion()
+                    else
+                        self:buildLayout()
+                    end
+                else
+                    self:alertError()
+                end
+            end
         })
+        self.buttons[i] = item
         self:addChild(item)
     end
 end
