eeaae561690ab2b678e8ccbcb1a86c222e68b631
Hans Zhang <ezburg@gmail.com>
2014-09-11 03:57:59 +0200

    add the pwd again

--- compile_scripts_config.php
+++ compile_scripts_config.php
@@ -3,5 +3,8 @@ return array(
 	'src'      => 'scripts',
 	'output'   => 'game.zip',
 	'compile'  => 'zip',
+	'encrypt'  => 'xxtea_zip',
+	'key'      => 'zhclovewrh',
+	'sign'     => 'encrypted',
 	'extname'  => 'lua'
 );
