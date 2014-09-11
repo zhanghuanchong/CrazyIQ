808dbef3cfd0c0c370a795a6169811069434509e
Hans Zhang <ezburg@gmail.com>
2014-09-11 03:02:41 +0200

    Added the script config file to compile and encrypt the code.

---
+++ compile_scripts_config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+	'src'      => 'scripts',
+	'output'   => 'game.zip',
+	'compile'  => 'zip',
+	'encrypt'  => 'xxtea_zip',
+	'key'      => 'zhclovewrh',
+	'sign'     => 'encrypted',
+	'extname'  => 'lua'
+);
